0cd08ab35270167f469e1778534798aa821b2601
HiImSpAkA <rafasamp2002@gmail.com>
2025-05-19 14:40:27 +0100

    Update vite.config.js

--- Frontend/Auditoria norma 27001/vite.config.js
+++ Frontend/Auditoria norma 27001/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -16,7 +16,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
   },
-  // Configurando o título da página
+  
   define: {
     'import.meta.env.VITE_APP_TITLE': JSON.stringify('Norma 27001'),
   },
